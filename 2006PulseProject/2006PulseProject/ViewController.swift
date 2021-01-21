9f646dc4fe9d00d54cc036f856d5555cd8d82a29
Manjunadh <manjunadhb@gmail.com>
2021-01-21 17:58:01 +0530

    Created a new project where other developers can clone.

---
+++ 2006PulseProject/2006PulseProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  2006PulseProject
+//
+//  Created by MacUser on 21/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var age:UInt8 = 20
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
